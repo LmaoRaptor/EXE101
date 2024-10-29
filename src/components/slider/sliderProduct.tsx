496bc06509b4871bf8789a4169d18e193dd7aec6
Cuongnc165 <cuongnc0503@gmail.com>
2024-10-29 12:20:13 +0700

    fix/fix-image

--- src/components/slider/sliderProduct.tsx
+++ src/components/slider/sliderProduct.tsx
@@ -28,36 +28,36 @@ const SliderProduct = ({ isHeading = false }: Props) => {
       <Slider {...settings}>
         <div className="pr-4 mr-[-1rem]">
           <img
-            className="w-full object-cover rounded-2xl h-96"
-            src="https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-44.jpg.webp"
+            className="w-full object-cover rounded-2xl h-96 shadow-lg"
+            src="https://cdn.tgdd.vn/Files/2019/10/30/1213974/nguyen-ly-hoat-dong-cua-may-anh-co-the-ban-chua-biet.jpg"
             alt="Slide 1"
           />
         </div>
         <div className="pr-4 mr-[-1rem]">
           <img
-            className="w-full object-cover rounded-2xl h-96"
-            src="https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-44.jpg.webp"
+            className="w-full object-cover rounded-2xl h-96 shadow-lg"
+            src="https://cdn.tgdd.vn/Products/Images/44/325242/dell-inspiron-15-3520-i5-n5i5052w1-thumb-600x600.jpg"
             alt="Slide 2"
           />
         </div>
         <div className="pr-4 mr-[-1rem]">
           <img
-            className="w-full object-cover rounded-2xl h-96"
-            src="https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-44.jpg.webp"
+            className="w-full object-cover rounded-2xl h-96 shadow-lg"
+            src="https://bizweb.dktcdn.net/100/412/747/products/fornix-ft26-trang.jpg?v=1696236458200"
             alt="Slide 3"
           />
         </div>
         <div className="pr-4 mr-[-1rem]">
           <img
-            className="w-full object-cover rounded-2xl h-96"
-            src="https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-44.jpg.webp"
+            className="w-full object-cover rounded-2xl h-96 shadow-lg"
+            src="https://nvs.tn-cdn.net/2022/01/0dea9651447c4d44927dee801583fcb0.png"
             alt="Slide 4"
           />
         </div>
         <div className="pr-4 mr-[-1rem]">
           <img
-            className="w-full object-cover rounded-2xl h-96"
-            src="https://noithatbinhminh.com.vn/wp-content/uploads/2022/08/anh-dep-44.jpg.webp"
+            className="w-full object-cover rounded-2xl h-96 shadow-lg"
+            src="https://minhancomputercdn.com/media/product/5397_chu___t_m__y_t__nh_logitech_gaming_g102_gen_2__black__02.jpg"
             alt="Slide 5"
           />
         </div>
